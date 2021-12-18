219192ce6bfa4a35ebdc314c4b0d6cf5983fc5a2
Lisha Wang <lishawang1020@gmail.com>
2021-12-17 18:19:37 -0800

    commit 2

--- server/schemas/favorite.schema.js
+++
@@ -1,9 +0,0 @@
-const Schema = require("mongoose").Schema;
-
-module.exports = new Schema(
-  {
-    username: { type: String, required: true },
-    jobId: { type: String, required: true },
-  },
-  { collection: "favorites" }
-);
--- server/schemas/user.schema.js
+++
@@ -1,9 +0,0 @@
-const Schema = require("mongoose").Schema;
-
-module.exports = new Schema(
-  {
-    username: { type: String, index: true, required: true },
-    password: { type: String, required: true },
-  },
-  { collection: "users" }
-);
